b1ef25192724bf228900c2e6ef68788864286119
Your Name <you@example.com>
2024-05-25 22:16:04 +0200

    added one more temp sensor

--- chartCreator.js
+++ chartCreator.js
@@ -11,6 +11,7 @@ export function prepareChart(dayData){
 
     var insideLogs = [];
     var outsideLogs = [];
+    var insideFloorLogs = [];
 
     splitted.forEach(json => {
 
@@ -22,20 +23,24 @@ export function prepareChart(dayData){
         if(object.name == 'outside'){
             outsideLogs.push(object)
         }
+        if(object.name == 'inside_floor'){
+            insideFloorLogs.push(object)
+        }
     });
 
     var labels = insideLogs.map(element => element.time)
     var insideData = insideLogs.map(element => element.temp)
     var outsideData = outsideLogs.map(element => element.temp)
+    var insideFloorLogs = insideFloorLogs.map(element => element.temp)
 
     // console.log("labels : " + labels)
     // console.log("insideData : " + insideData)
     // console.log("data2 : " + data2)
 
-    createChart(labels, insideData, outsideData)
+    createChart(labels, insideData, outsideData, insideFloorLogs)
 }
 
-function createChart(labels, data1, data2){
+function createChart(labels, data1, data2, data3){
 
     var ctx = document.getElementById('myChart').getContext('2d');
     Chart.defaults.font.size = 14;
@@ -49,10 +54,10 @@ function createChart(labels, data1, data2){
                 // data: [12, 19, 3, 5, 2, 3],
                 data: data1,
                 backgroundColor: [
-                    'rgba(255, 130, 50, 0.2)'
+                    'rgba(255, 100, 50, 0.2)'
                 ],
                 borderColor: [
-                    'rgba(255, 130, 50, 1)'
+                    'rgba(255, 100, 50, 1)'
                 ],
                 borderWidth: 1,
                 tension: 0.3,
@@ -66,6 +71,19 @@ function createChart(labels, data1, data2){
                 ],
                 borderColor: [
                     'rgba(99, 220, 132, 1)'
+                ],
+                borderWidth: 1,
+                tension: 0.3,
+                fill: true
+            },{
+                label: 'inside_floor',
+                // data: [12, 19, 3, 5, 2, 3],
+                data: data3,
+                backgroundColor: [
+                    'rgba(255, 190, 50, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 190, 50, 1)'
                 ],
                 borderWidth: 1,
                 tension: 0.3,
